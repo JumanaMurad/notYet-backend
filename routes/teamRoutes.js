8c513978b643df685719506e9839421f7cd4babd
rabehmohamed <81047812+rabehmohamed@users.noreply.github.com>
2023-03-26 00:09:19 +0200

    Add files via upload

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const teamController = require('../controllers/teamController');
 const authController = require("../controllers/authController");
+const teamReqController = require("../controllers/teamJoinRequestController");
 const router = express.Router();
 
 
@@ -9,12 +10,15 @@ router
     .route('/')
     .get(authController.protect , teamController.getAllTeams)
     .post(authController.protect, teamController.createTeam);
-
+    
 router
     .route('/:id')
     .patch(authController.protect,teamController.updateTeam)
     .get(authController.protect,teamController.getTeam)
-    .delete(authController.protect ,teamController.DeleteTeam);
+    .delete(authController.protect ,teamController.DeleteTeam)
+    .post(authController.protect , authController.restrictTo("user"),teamReqController.requestToJoin)
+    .post(authController.protect , authController.restrictTo("team-leader"),teamReqController.approveRequestToJoin)
+    .post(authController.protect , authController.restrictTo("team-leader"),teamReqController.rejectRequestToJoin);
 
 
 module.exports = router;
