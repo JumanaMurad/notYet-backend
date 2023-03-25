b508c9dc63c3180b64bed591ea873f747fe1ab8c
rabehmohamed <81047812+rabehmohamed@users.noreply.github.com>
2023-03-26 00:10:29 +0200

    Add files via upload

--- models/teamJoinRequestModel.js
+++ models/teamJoinRequestModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
 
 const teamJoinRequestSchema = mongoose.Schema({
     user: {
-        type: Schema.Types.ObjectId,
+        type: [Schema.Types.ObjectId],
         ref: 'User',
         required: true
     },
