2852eff05d990b5beb35af8050504639859ff8f8
mahmudhmh <mahmoudh.buss@gmail.com>
2023-05-23 19:09:34 +0300

    Problems data auth

--- routes/problemRoutes.js
+++ routes/problemRoutes.js
@@ -1,22 +1,35 @@
-const express = require('express');
-const problemsController = require('../controllers/problemController');
-const authController = require('../controllers/authController');
+const express = require("express");
+const problemsController = require("../controllers/problemController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
+router
+  .route("/")
+  .get(problemsController.getAllProblems)
+  .post(
+    authController.protect,
+    authController.restrictTo("admin"),
+    problemsController.createProblem
+  );
 
 router
-    .route('/')    
-    .get(authController.protect, problemsController.getAllProblems)
-    .post(authController.protect , authController.restrictTo('admin') ,problemsController.createProblem);
+  .route("/:id")
+  .patch(
+    authController.protect,
+    authController.restrictTo("admin"),
+    problemsController.updateProblem
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin", "team-leader"),
+    problemsController.deleteProblem
+  )
+  .get(authController.protect, problemsController.getProblem);
 
-
-router
-    .route('/:id')
-    .patch(authController.protect , authController.restrictTo("admin") , problemsController.updateProblem)   
-    .delete(authController.protect,authController.restrictTo('admin' , 'team-leader') ,problemsController.deleteProblem)
-    .get(authController.protect, problemsController.getProblem);
-    
-
-router.get('/getSolvedProblems',authController.protect,problemsController.getSolvedProblems);
+router.get(
+  "/getSolvedProblems",
+  authController.protect,
+  problemsController.getSolvedProblems
+);
 
 module.exports = router;
