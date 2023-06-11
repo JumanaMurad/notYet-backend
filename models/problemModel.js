035243878b7353e6cf455fd6b1750c2901672155
JumanaMurad <jumanamurad1@gmail.com>
2023-06-11 14:28:45 +0300

    problem contraints

--- models/problemModel.js
+++ models/problemModel.js
@@ -3,60 +3,63 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const problemSchema = new Schema({
-     title: {
-        type: String,
-        required : true,
-        unique: true,
-        trim: true
-     },
-     description : {
-        type: String,
-        required : true
-     },
-     category : {
-        type : String,
-        required: true
-     },
-     rank : {
-        type : String,
-        required: true
-     },
-     difficulty: {
-        type : String,
-        required : true,
-        enum: [
+   title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+   },
+   description: {
+      type: String,
+      required: true
+   },
+   category: {
+      type: String,
+      required: true
+   },
+   rank: {
+      type: String,
+      required: true
+   },
+   difficulty: {
+      type: String,
+      required: true,
+      enum: [
          "Easy",
          "Medium",
          "Hard"
-        ]
-     },
-     inputs: [{
+      ]
+   },
+   inputs: [{
       type: String
-     }],
-     outputs: [{
+   }],
+   outputs: [{
       type: String
-     }],
-     hiddenInputs: [{
+   }],
+   hiddenInputs: [{
       type: String
-     }],
-     hddenOutputs: [{
+   }],
+   hiddenOutputs: [{
       type: String
-     }],
-     numberOfSolutions: {
+   }],
+   constraints: [{
+      type: String
+   }],
+   numberOfSolutions: {
       type: Number,
       default: 0
-     },
-     // Consider it to be easier for hard delete
-     contests: {
+   },
+   // Consider it to be easier for hard delete
+   contests: {
       type: [Schema.Types.ObjectId],
       ref: 'Contest'
-     },
-     users: {
+   },
+   users: {
       type: [Schema.Types.ObjectId],
       ref: 'User'
-     }
+   }
 });
 
-const Problem = mongoose.model('Problem' , problemSchema);
+const Problem = mongoose.model('Problem', problemSchema);
 
 module.exports = Problem
